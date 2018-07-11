6eb8b1161e388993323fe8c2abad2395b79af894
101testcoding <101testcoding@gmail.com>
2018-07-11 16:47:20 +0800

    updated routes with callback

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,28 @@
 class User < ApplicationRecord
   include Clearance::User
+  # mount_uploader :avatar, AvatarUploader
+  has_many :authentications, dependent: :destroy
+  has_many :listings
+  has_many :reservations
+  has_many :listings, through: :reservations
 
-  include Clearance::User
+  enum role: [ :customer, :superadmin, :moderator]
+
+	def self.create_with_auth_and_hash(authentication, auth_hash)
+		user = self.create!(
+		first_name: auth_hash["info"]["first_name"],
+		last_name: auth_hash["info"]["last_name"],
+		email: auth_hash["info"]["email"],
+    password: SecureRandom.hex(10)
+		)
+		user.authentications << authentication
+		return user
+	end
+
+	# grab google to access google for user data
+	def google_token
+		x = self.authentications.find_by(provider: 'google_oauth2')
+		return x.token unless x.nil?
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+  get 'welcome/index'
   resources :passwords, controller: "clearance/passwords", only: [:create, :new]
   resource :session, controller: "clearance/sessions", only: [:create]
 
@@ -12,4 +13,13 @@ Rails.application.routes.draw do
   get "/sign_in" => "clearance/sessions#new", as: "sign_in"
   delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
   get "/sign_up" => "clearance/users#new", as: "sign_up"
+  get "/auth/:provider/callback" => "sessions#create_from_omniauth"
+
+
+  
+root "welcome#index"
 end
+
+
+
+
